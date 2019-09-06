e9cff9d9d5478f2776e0ac47912120109ea57dcc
Dionino <51341014+Dionino@users.noreply.github.com>
2019-09-06 21:49:29 +0100

    Script jQuery
    added script.js in jQuery

---
+++ js/scripts.js
@@ -0,0 +1,114 @@
+var digimonList = (function () {
+  var repository = [];
+
+  var apiUrl = 'https://digimon-api.herokuapp.com/api/digimon';
+
+  function showDetails(item) {
+    digimonList.loadDetails(item).then(function (digimon) {
+      showModal(digimon);
+    })
+  };
+
+  function addListItem(digimon) {
+    var $listItem = $('Digilist');
+    $('Digilist').append('<li><button class="digimon_button">' + digimon.name + '</button></li>');
+    $('body').append($listItem);
+    $('button').on('click', function (event) {
+      showDetails(digimon);
+    })
+  };
+
+  function getAll() {
+    return repository;
+  }
+
+  function add(item) {
+    repository.push(item);
+  }
+
+  function loadList() {
+    return $.ajax(apiUrl, {dataType: 'json'}).then(function (item) {
+      $.each(item.results, function(i, item) {
+        var digimon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(digimon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
+  };
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return $.ajax(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+      return item
+    }).catch(function (e) {
+      console.error(e);
+    });
+  };
+
+  function showModal(digimon) {
+    var $modalDigimon = $('#modal-digimon').html('');
+    var $modal = $('<div class="modal"></div>');
+    var $closeButtonElement = $('<button class="modal-close">Close</button>');
+    $closeButtonElement.on('click', hideModal);
+    var $nameElement = $('<h1>' + pokemon.name + ' </h1>');
+    var $imageElement = $('<img src="' + imageUrl + '"></img>');
+    var $heightElement = $('<p>' + pokemon.height + ' </p>');
+
+    $modal.append($closeButtonElement);
+    $modal.append($nameElement);
+    $modal.append($imageElement);
+    $modal.append($heightElement);
+    $('#modal-digimon').append($modal);
+
+    $('#modal-digimon').addClass('is-visible');
+  };
+
+  function hideModal() {
+    var $modalDigimon = $('#modal-digimon');
+    $('#modal-digimon').removeclass('is-visible');
+  };
+
+  $(window).on('keydown', (e) => {
+    var $modalDigimon = $('#modal-digimon');
+    if (e.key === 'Escape' && $('#modal-digimon').hasClass('is-visible')) {
+      hideModal();
+    }
+  });
+
+  var $modalDigimon = $('#modal-digimon');
+  $('#modal-digimon').on('click', (e) => {
+    var target = e.target;
+    if (target === $modalDigimon) {
+      hideModal();
+    }
+  });
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
+  };
+
+})(); // IIFE ends here
+
+var $Digilist = $('Digilist');
+
+digimonList.loadList().then(function(digimon) {
+  var $digimon = digimonList.getAll();
+  $.each($digimon, function(i, digimon) {
+    addListItem(digimon);
+  });
+});
