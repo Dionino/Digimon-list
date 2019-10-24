53810a9ff14b7f84680938ac8ddc4a25642756c6
Dionino <51341014+Dionino@users.noreply.github.com>
2019-10-24 18:23:14 +0100

    Bootstrap changes
    async, await added. style buttons changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,82 @@
 var digimonList = (function () {
-  var repository = [];
+	var repository = [];
 
-  var apiUrl = 'https://digimon-api.herokuapp.com/api/digimon';
+	var apiUrl = 'https://digimon-api.herokuapp.com/api/digimon';
 
-  function showDetails(item) {
-    digimonList.loadDetails(item).then(function (digimon) {
-      showModal(digimon);
-    })
-  };
+	function showDetails(item) {
+		digimonList.loadDetails(item).then(function (digimon) {
+			showModal(digimon);
+		})
+	}
 
-  function addListItem(digimon) {
-    var $listItem = $('.Digilist');
-    var $button = $('<li><button type="button" class="digimon_button" data-toggle="modal" data-target="#exampleModalCenter"> ' + digimon.name + '</button></li>');
-    $listItem.append($button);
-    $('body').append($listItem);
-    $listItem.click(function (event) {
-      showDetails(digimon);
-    })
-  };
+	function addListItem(digimon) {
+		var $listItem = $('.Digilist');
+		var $button = $('<li><button type="button" class="digimon_button" data-toggle="modal" data-target="#exampleModalCenter"> ' + digimon.name + '</button></li>');
+		$listItem.append($button);
+		$('body').append($listItem);
+		$listItem.click(function () {
+			showDetails(digimon);
+		})
+	}
 
-  function getAll() {
-    return repository;
-  }
+	function getAll() {
+		return repository;
+	}
 
-  function add(item) {
-    repository.push(item);
-  }
+	function add(item) {
+		repository.push(item);
+	}
 
-  function loadList() {
-    return $.ajax({ url: apiUrl, dataType: 'json'}).then(function (item) {
-      $.each(item, function(i, item) {
-        add(item);
-      });
-    }).catch(function(e) {
-      console.error(e);
-    })
-  };
+	async function loadList() {
+		return await $.ajax({ url: apiUrl, dataType: 'json'}).then(function (item) {
+			$.each(item, function(i, item) {
+				add(item);
+			});
+		}).catch(function(e) {
+			console.error(e);
+		})
+	}
 
-  function loadDetails(item) {
-    var url = 'https://digimon-api.herokuapp.com/api/digimon';
-    return $.ajax(url).then(function (details) {
-      $.each(item, function(i, item) {
-        add(item);
-      });
-      return item;
-    }).catch(function (e) {
-      console.error(e);
-    });
-  };
+	async function loadDetails(item) {
+		var url = 'https://digimon-api.herokuapp.com/api/digimon';
+		return await $.ajax(url).then(function () {
+			$.each(item, function(i, item) {
+				add(item);
+			});
+			return item;
+		}).catch(function (e) {
+			console.error(e);
+		});
+	}
 
-  function showModal(digimon) {
-    var $modalTitle = $('.modal-header');
-    var $nameElement = $('<h5>' + digimon.name + ' </h5>');
-    $modalTitle.append('$nameElement');
-    var $modal = $('modal-body');
-    var $imageElement = $('<img src="https://digimon.shadowsmith.com/img/'+digimon.name.toLowerCase()+'.jpg"></img>');
-    $('div.digimon-img').html($imageElement);
-    var $detailsElement = $('<p> ID: ' + digimon.id + ', Level: ' + digimon.level + ' </p>');
-    $modal.append('$imageElement');
-    $modal.append('$detailsElement');
+	function showModal(digimon) {
+		var $modalTitle = $('.modal-header');
+		var $nameElement = $('<h5>' + digimon.name + ' </h5>');
+		$modalTitle.append('$nameElement');
+		var $modal = $('modal-body');
+		var $imageElement = $('<img src="https://digimon.shadowsmith.com/img/'+digimon.name.toLowerCase()+'.jpg"></img>');
+		$('div.digimon-img').html($imageElement);
+		var $detailsElement = $('<p> ID: ' + digimon.id + ', Level: ' + digimon.level + ' </p>');
+		$modal.append('$imageElement');
+		$modal.append('$detailsElement');
 
-  };
+	}
 
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-  };
+	return {
+		add: add,
+		getAll: getAll,
+		addListItem: addListItem,
+		loadList: loadList,
+		loadDetails: loadDetails,
+	};
 
 })(); // IIFE ends here
 
 var $Digilist = $('Digilist');
 
 digimonList.loadList().then(function() {
-  var $digimon = digimonList.getAll();
-  $.each($digimon, function(i, digimon) {
-    digimonList.addListItem(digimon);
-  });
+	var $digimon = digimonList.getAll();
+	$.each($digimon, function(i, digimon) {
+		digimonList.addListItem(digimon);
+	});
 });
